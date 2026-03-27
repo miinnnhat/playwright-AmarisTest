c554d9f651a125aa5a465693c34043456d95585d
miinnnhat <131011057+miinnnhat@users.noreply.github.com>
2026-03-27 13:30:31 +0700

    Create functions
    Function input Name Senquenly
    update json file
    testcase can fill name into search box

--- pages/homePage.pom.js
+++ pages/homePage.pom.js
@@ -1,16 +1,24 @@
 import { get } from "node:http";
 
+//Locators 
 export class HomePage {
     constructor(page){
         this.page = page;
 
         this.searchInput =page.getByTestId("textInput");
-        this.
+        
 
     }
 
-
+// Method to open the home page
     async open(){
         await this.page.goto('/');
+    }
+
+// Method to input hotel name in search box
+    async inputHotelName(testData){
+        await this.searchInput.pressSequentially(testData.hotelName, {delay: 100});
+
+        // const searchResults = this.page.
     }
 }
--- tests/home-search.spec.js
+++ tests/home-search.spec.js
@@ -1,5 +1,6 @@
 import {test, expect} from '@playwright/test';
 import {HomePage} from '../pages/homePage.pom.js';
+import testData from '../data/testData.json';
 
 test.describe('Home Search Tests', () =>{
     test('Access Home Page and Search for a Hotel', async ({page}) =>{
@@ -8,5 +9,13 @@ test.describe('Home Search Tests', () =>{
         await homePage.open();
         await expect(page).toHaveTitle(/Agoda/i);
         await expect(page).toHaveURL(/agoda\.com/);
+    });
+
+    test('Verify that user can field hotel name in search box', async ({page}) =>{
+
+        const homePage = new HomePage(page);
+        await homePage.open();
+        await homePage.searchInput.inputHotelName(testData[0]);
+        await expect(homePage.searchInput).toHaveValue(testData[0].hotelName);
     });
 });
